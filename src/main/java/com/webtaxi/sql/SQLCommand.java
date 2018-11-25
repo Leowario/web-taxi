f32fe6f833afb7111b10cfd0402cddfe5f3fe378
Leowario <voronventusa@gmail.com>
2018-11-25 22:56:08 +0200

    Commands for route history added

--- src/main/java/com/webtaxi/sql/SQLCommand.java
+++ src/main/java/com/webtaxi/sql/SQLCommand.java
@@ -35,13 +35,22 @@ public enum SQLCommand {
             "FROM web_taxi.drivers " +
             "where " +
             "is_free = true AND car_class = ?"),
-    SELECT_ROUTE_HISTORY_OF_CUSTOMER("SELECT start_pont, end_point," +
+    CREATE_TABLE_ROUTE_HISTORY("CREATE TABLE IF NOT EXISTS route_history(" +
+            "route_id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+            "customer_id int ," +
+            "FOREIGN KEY (customer_id) references customers(cust_id) on delete cascade," +
+            "driver_id   int ," +
+            "FOREIGN KEY (driver_id) references drivers(driv_id) on delete cascade," +
+            "start_pint  VARCHAR(255) NOT NULL," +
+            "end_point   VARCHAR(255) NOT NULL)"),
+    SELECT_ROUTE_HISTORY_OF_CUSTOMER("SELECT start_point, end_point," +
             "drivers.first_name,drivers.last_name," +
             "drivers.car_model,drivers.car_class," +
             "drivers.rating " +
             "FROM web_taxi.route_history AS rh, web_taxi.drivers " +
             "WHERE rh.customer_id = ? " +
-            "AND rh.driver_id = web_taxi.drivers.driv_id");
+            "AND rh.driver_id = web_taxi.drivers.driv_id"),
+    ADD_ROUTE_TO_ROUTE_HISTORY("INSERT INTO route_history(customer_id,driver_id,stat_point,end_point) VALUES (?,?,?,?");
 
     private final String sql;
 
