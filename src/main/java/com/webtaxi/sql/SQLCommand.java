d5d68bd55d15206acdf3d03a7c0b8467c57ea1c7
Leowario <voronventusa@gmail.com>
2018-11-27 22:37:04 +0200

    Added commands for work with route history

--- src/main/java/com/webtaxi/sql/SQLCommand.java
+++ src/main/java/com/webtaxi/sql/SQLCommand.java
@@ -19,7 +19,6 @@ public enum SQLCommand {
             "rating INT(10))"),
     CREATE_TABLE_DRIVERS("CREATE TABLE IF NOT EXISTS drivers(" +
             "driv_id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-
             "first_name VARCHAR(255) NOT NULL UNIQUE," +
             "last_name VARCHAR(255) NOT NULL UNIQUE," +
             "car_model VARCHAR(255) NOT NULL," +
@@ -27,7 +26,7 @@ public enum SQLCommand {
             "rating INT(10) NOT NULL," +
             "is_free BOOLEAN)"),
     SELECT_ALL_FROM_CUSTOMERS("SELECT * FROM customers"),
-    SELECT_CUSTOMER_BY_LOGIN_PASSWORD("SELECT id, login, first_name,last_name,rating FROM web_taxi.customers " +
+    SELECT_CUSTOMER_BY_LOGIN_PASSWORD("SELECT cust_id, login, first_name,last_name,rating FROM web_taxi.customers " +
             "WHERE login = ? and password = ?"),
     DELETE_USER_BY_LOGIN("DELETE FROM  web_taxi.customers WHERE login=?"),
     SELECT_ALL_FROM_DRIVERS("SELECT * FROM drivers"),
@@ -37,21 +36,20 @@ public enum SQLCommand {
             "is_free = true AND car_class = ?"),
     CREATE_TABLE_ROUTE_HISTORY("CREATE TABLE IF NOT EXISTS route_history(" +
             "route_id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-            "customer_id int ," +
+            "customer_id int," +
             "FOREIGN KEY (customer_id) references customers(cust_id) on delete cascade," +
-            "driver_id   int ," +
+            "driver_id   int," +
             "FOREIGN KEY (driver_id) references drivers(driv_id) on delete cascade," +
             "start_point  VARCHAR(255) NOT NULL," +
             "end_point   VARCHAR(255) NOT NULL)"),
-    SELECT_ROUTE_HISTORY_OF_CUSTOMER("SELECT start_point, end_point," +
-            "drivers.first_name,drivers.last_name," +
-            "drivers.car_model,drivers.car_class," +
-            "drivers.rating " +
-            "FROM web_taxi.route_history AS rh, web_taxi.drivers " +
-            "WHERE rh.customer_id = ? " +
-            "AND rh.driver_id = web_taxi.drivers.driv_id"),
-    ADD_ROUTE_TO_ROUTE_HISTORY("INSERT INTO route_history(customer_id,driver_id,start_point,end_point) VALUES (?,?,?,?");
-
+    SELECT_ROUTE_HISTORY_OF_CUSTOMER("SELECT start_point, end_point,drivers.first_name," +
+            "drivers.last_name ,drivers.rating," +
+            " drivers.car_model," +
+            "drivers.car_class FROM route_history , drivers " +
+            "WHERE route_history.customer_id = ? " +
+            "AND route_history.driver_id = drivers.driv_id"),
+    ADD_ROUTE_TO_ROUTE_HISTORY("INSERT INTO route_history(customer_id,driver_id,start_point,end_point) VALUES (?,?,?,?)"),
+    DELETE_ROUTE_BY_ID("DELETE FROM route_history where start_point= ?");
     private final String sql;
 
     public String sql() {
