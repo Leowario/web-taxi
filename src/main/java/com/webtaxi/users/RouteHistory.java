d1a15aa01cab31073e031b8ff2c46fc5ba6c714a
Leowario <voronventusa@gmail.com>
2018-11-27 22:35:35 +0200

    Route History created

---
+++ src/main/java/com/webtaxi/users/RouteHistory.java
@@ -0,0 +1,31 @@
+package com.webtaxi.users;
+
+/**
+ * @author Vitalii Usatyi
+ * <p>
+ * It's form for showing route to customer
+ */
+public class RouteHistory {
+    private String startPoint;
+    private String endPoint;
+    private Driver driver;
+
+
+    RouteHistory(String startPoint, String endPoint, Driver driver) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.driver = driver;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+}
