331865ee4d6975cc11b8c7af6693aa80d37e03d8
Leowario <voronventusa@gmail.com>
2018-11-27 22:36:01 +0200

    Route History Factory created

---
+++ src/main/java/com/webtaxi/users/RouteHistoryFactory.java
@@ -0,0 +1,27 @@
+package com.webtaxi.users;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vitalii Usatyy
+ */
+public class RouteHistoryFactory {
+
+    private RouteHistoryFactory() {
+
+    }
+
+    public static List<RouteHistory> getRouteList(ResultSet resultSet) throws SQLException {
+        List<RouteHistory> routeHistoryList = new ArrayList<>();
+        while (resultSet.next()) {
+            String start_point = resultSet.getString("start_point");
+            String end_point = resultSet.getString("end_point");
+            Driver driver = DriverFactory.createDriver(resultSet);
+            routeHistoryList.add(new RouteHistory(start_point, end_point, driver));
+        }
+        return routeHistoryList;
+    }
+}
