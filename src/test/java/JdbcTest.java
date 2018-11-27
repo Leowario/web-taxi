00cc3ebdd86440c0c5d7ac5794dd0d2ca75be3dc
Leowario <voronventusa@gmail.com>
2018-11-27 22:35:04 +0200

    Unit tests created and corrected

--- src/test/java/JdbcTest.java
+++ src/test/java/JdbcTest.java
@@ -1,11 +1,14 @@
 import com.webtaxi.sql.SQLExecutor;
 import com.webtaxi.users.Customer;
 import com.webtaxi.users.Driver;
+import com.webtaxi.users.Route;
+import com.webtaxi.users.RouteHistory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 import java.util.PriorityQueue;
 
@@ -17,7 +20,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @DisplayName("JDBC API should:")
 class JdbcTest {
     @Test
-    @DisplayName("get User by login")
+    @DisplayName("get User by login and password")
     void getUserByLoginAndPassword() throws SQLException {
         Customer roman = Customer.builder()
                 .setLogin("roma")
@@ -26,6 +29,7 @@ class JdbcTest {
                 .setLastName("Momanov")
                 .setRating(5)
                 .build();
+        SQLExecutor.deleteCustomerByLogin("roma");
         SQLExecutor.addCustomer(roman);
         Optional<Customer> optional = SQLExecutor.selectCustomerByLoginAndPassword("roma", "123abc");
         if (optional.isPresent()) {
@@ -33,6 +37,7 @@ class JdbcTest {
             Assertions.assertEquals("Roman", customer.getFirstName());
             Assertions.assertEquals("Momanov", customer.getLastName());
             Assertions.assertEquals("roma", customer.getLogin());
+            System.out.println(customer.toString());
         } else {
             throw new SQLException();
         }
@@ -49,11 +54,36 @@ class JdbcTest {
         PriorityQueue<Driver> drivers = optional.get();
         while (drivers.iterator().hasNext()) {
             Driver currentDriver = checkNotNull(drivers.poll());
+            Assertions.assertEquals("standard", currentDriver.getCar().getCarClass());
             System.out.print(currentDriver.getFirstName() + " || ");
             System.out.print(currentDriver.getCar().getCarClass() + " || ");
             System.out.print(currentDriver.getRating() + " || ");
             System.out.println();
         }
     }
-}
 
+    @Test
+    @DisplayName("get all route from route history")
+    void getAllRouteFromRouteHistory() throws SQLException {
+        String startPoint = "test";
+        String endPoint = "test";
+        Route newRote = Route.builder()
+                .setCustomerId(9)
+                .setDriverId(1)
+                .setStartPoint(startPoint)
+                .setEndPoint(endPoint)
+                .build();
+        SQLExecutor.addRouteToRouteHistory(newRote);
+        Optional<List<RouteHistory>> optional = SQLExecutor.selectAllRouteHistoryOfCustomer(9);
+        if (!optional.isPresent()) {
+            throw new SQLException();
+        }
+        List<RouteHistory> routeHistories = optional.get();
+        for (RouteHistory route : routeHistories) {
+            Assertions.assertEquals(startPoint, route.getStartPoint());
+            Assertions.assertEquals(endPoint, route.getEndPoint());
+            System.out.println(route.getStartPoint() + "  " + route.getEndPoint() + " " + route.getDriver().toString());
+        }
+        SQLExecutor.deleteRouteByStartPoint("test");
+    }
+}
