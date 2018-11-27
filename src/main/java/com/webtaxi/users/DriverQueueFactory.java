62ba5e195b52e27dab4e9dc3eab477c489730898
Leowario <voronventusa@gmail.com>
2018-11-27 22:31:47 +0200

    Driver creation changed to Factory

--- src/main/java/com/webtaxi/users/DriverQueueFactory.java
+++ src/main/java/com/webtaxi/users/DriverQueueFactory.java
@@ -8,6 +8,7 @@ import java.util.PriorityQueue;
  * @author Vitalii Usatyi
  */
 public class DriverQueueFactory {
+
     private DriverQueueFactory() {
 
     }
@@ -19,15 +20,7 @@ public class DriverQueueFactory {
     public static PriorityQueue<Driver> getDriversQueue(ResultSet resultSet) throws SQLException {
         PriorityQueue<Driver> driversQueue = new PriorityQueue<>();
         while (resultSet.next()) {
-            Driver driver = Driver.builder()
-                    .setFirstName(resultSet.getString("first_name"))
-                    .setLastName(resultSet.getString("last_name"))
-                    .setCar(new Car(
-                            resultSet.getString("car_model"),
-                            resultSet.getString("car_class")
-                    ))
-                    .setRating(resultSet.getInt("rating"))
-                    .build();
+            Driver driver = DriverFactory.createDriver(resultSet);
             driversQueue.add(driver);
         }
         return driversQueue;
