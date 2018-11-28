f6b27893237e7fe62ab71ca04ba64fa26f9e82a4
Leowario <voronventusa@gmail.com>
2018-11-28 20:48:17 +0200

    Created classes for working with each table

---
+++ src/main/java/com/webtaxi/sql/SQLCustomerCommandExecutor.java
@@ -0,0 +1,81 @@
+package com.webtaxi.sql;
+
+import com.webtaxi.users.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+
+import static com.webtaxi.sql.SQLCommand.*;
+import static com.webtaxi.sql.SQLStatementFactory.getPreparedStatement;
+import static com.webtaxi.sql.SQLStatementFactory.getStatement;
+import static com.webtaxi.users.CustomerFactory.createCustomer;
+
+/**
+ * @author Vitalii Usatyi
+ */
+public class SQLCustomerCommandExecutor extends SQLExecutor {
+
+    private SQLCustomerCommandExecutor() {
+
+    }
+
+    public static void createTableCustomers() {
+        String sqlCommand = CREATE_TABLE_CUSTOMERS.sql();
+        try (Statement statement = getStatement()) {
+            statement.execute(sqlCommand);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addCustomer(Customer customer) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(ADD_CUSTOMER.sql())) {
+            int index = 1;
+            preparedStatement.setString(index, customer.getLogin());
+            preparedStatement.setString(++index, customer.getPassword());
+            preparedStatement.setString(++index, customer.getFirstName());
+            preparedStatement.setString(++index, customer.getLastName());
+            preparedStatement.setInt(++index, customer.getRating());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Optional<Customer> selectCustomerByLoginAndPassword(String login, String password) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(SELECT_CUSTOMER_BY_LOGIN_PASSWORD.sql())) {
+            int index = 1;
+            preparedStatement.setString(index, login);
+            preparedStatement.setString(++index, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            Customer customer = createCustomer(resultSet);
+            return Optional.of(customer);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public static void deleteCustomerByLogin(String login) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(DELETE_USER_BY_LOGIN.sql())) {
+            int index = 1;
+            preparedStatement.setString(index, login);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectAllFromCustomers() {
+        try (Statement statement = getStatement();
+             ResultSet resultSet = statement.executeQuery(SELECT_ALL_FROM_CUSTOMERS.sql())) {
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/com/webtaxi/sql/SQLRouteHistoryCommandExecutor.java
@@ -0,0 +1,70 @@
+package com.webtaxi.sql;
+
+import com.webtaxi.users.Route;
+import com.webtaxi.users.RouteHistory;
+import com.webtaxi.users.RouteHistoryFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+import static com.webtaxi.sql.SQLCommand.*;
+import static com.webtaxi.sql.SQLStatementFactory.getPreparedStatement;
+import static com.webtaxi.sql.SQLStatementFactory.getStatement;
+
+/**
+ * @author Vitalii Usatyi
+ */
+public class SQLRouteHistoryCommandExecutor extends SQLExecutor {
+
+    private SQLRouteHistoryCommandExecutor() {
+
+    }
+
+    public static void createTableRouteHistory() {
+        try (Statement statement = getStatement()) {
+            statement.execute(CREATE_TABLE_ROUTE_HISTORY.sql());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addRouteToRouteHistory(Route route) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(ADD_ROUTE_TO_ROUTE_HISTORY.sql())) {
+            int index = 1;
+            preparedStatement.setInt(index, route.getCustomerId());
+            preparedStatement.setInt(++index, route.getDriverId());
+            preparedStatement.setString(++index, route.getStartPoint());
+            preparedStatement.setString(++index, route.getEndPoint());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Optional<List<RouteHistory>> selectAllRouteHistoryOfCustomer(int customerId) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(SELECT_ROUTE_HISTORY_OF_CUSTOMER.sql())) {
+            int index = 1;
+            preparedStatement.setInt(index, customerId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<RouteHistory> routeList = RouteHistoryFactory.getRouteList(resultSet);
+            return Optional.of(routeList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public static void deleteRouteByStartPoint(String startPoint) {
+        try (PreparedStatement preparedStatement = getPreparedStatement(DELETE_ROUTE_BY_ID.sql())) {
+            int index = 1;
+            preparedStatement.setString(index, startPoint);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
