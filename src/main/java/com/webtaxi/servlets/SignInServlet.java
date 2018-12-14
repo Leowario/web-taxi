76ef927d5478a31be52c57a32037c814ba1686d5
Leowario <voronventusa@gmail.com>
2018-12-14 21:30:23 +0200

    Servlet method changed to post, AJAX part added

--- src/main/java/com/webtaxi/servlets/SignInServlet.java
+++ src/main/java/com/webtaxi/servlets/SignInServlet.java
@@ -1,19 +1,14 @@
 package com.webtaxi.servlets;
 
-import com.google.common.net.MediaType;
 import com.webtaxi.users.Customer;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Optional;
 
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static com.webtaxi.sql.SQLCustomerCommandExecutor.selectCustomerByLoginAndPassword;
 
 @WebServlet(
@@ -22,17 +17,18 @@ import static com.webtaxi.sql.SQLCustomerCommandExecutor.selectCustomerByLoginAn
 )
 public class SignInServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
         Optional<Customer> optional = selectCustomerByLoginAndPassword(login, password);
         if (optional.isPresent()) {
-            req.setAttribute("optional", optional.get());
-            RequestDispatcher view = req.getRequestDispatcher("customerProfile.jsp");
-            view.forward(req, resp);
+            resp.setContentType("text/plain");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write("Success");
         } else {
-            resp.setContentType("text/html");
-            //TODO inform user that login and password is incorrect
+            resp.setContentType("text/plain");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write("Incorrect login or password");
         }
     }
 }
