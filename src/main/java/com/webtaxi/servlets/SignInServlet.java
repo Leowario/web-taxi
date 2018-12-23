4352d99bc6c93d5f00db0569f0f2f51b9681be0d
Leowario <voronventusa@gmail.com>
2018-12-23 19:41:22 +0200

    JSP changed to Servlet and AJAX technology

--- src/main/java/com/webtaxi/servlets/SignInServlet.java
+++ src/main/java/com/webtaxi/servlets/SignInServlet.java
@@ -4,15 +4,14 @@ import com.webtaxi.users.Customer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-import static com.webtaxi.servlets.ResponseContentTypeSetter.setContentType;
-import static com.webtaxi.sql.SQLCustomerCommandExecutor.selectCustomerByLoginAndPassword;
+import static com.webtaxi.servlets.ResponseContentTypeSetter.setTextType;
+import static com.webtaxi.sql.SQLCustomerFacade.selectCustomerByLoginAndPassword;
 
 /**
  * @author Vitalii Usatyi
@@ -29,13 +28,10 @@ public class SignInServlet extends HttpServlet {
         Optional<Customer> optional = selectCustomerByLoginAndPassword(login, password);
         if (optional.isPresent()) {
             req.setAttribute("customer", optional.get());
-            setContentType(resp);
+            setTextType(resp);
             resp.getWriter().write("Success");
-            resp.addCookie(new Cookie("login", login));
-            resp.addCookie(new Cookie("password", password));
         } else {
-            System.out.println("lol");
-            setContentType(resp);
+            setTextType(resp);
             resp.getWriter().write("Incorrect login or password");
         }
     }
