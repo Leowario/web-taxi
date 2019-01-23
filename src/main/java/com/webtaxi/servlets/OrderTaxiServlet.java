0f8ad954d2a91685a74e941325d7504e82bdbc36
Leowario <voronventusa@gmail.com>
2019-01-23 18:32:07 +0200

    internal logic added

--- src/main/java/com/webtaxi/servlets/OrderTaxiServlet.java
+++ src/main/java/com/webtaxi/servlets/OrderTaxiServlet.java
@@ -1,5 +1,8 @@
 package com.webtaxi.servlets;
 
+import com.webtaxi.model.Route;
+import com.webtaxi.sql.SQLRouteHistoryFacade;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +13,28 @@ import java.io.IOException;
 import static com.webtaxi.servlets.ResponseContentTypeSetter.setTextType;
 
 @WebServlet(
-        name = "OrderTaxi",
+        name = "orderTaxi",
         urlPatterns = "/orderTaxi"
 )
 public class OrderTaxiServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int customerId = Integer.parseInt(req.getParameter("customerId"));
+        int driverId = Integer.parseInt(req.getParameter("driverId"));
+        String startPoint = req.getParameter("startPoint");
+        String endPoint = req.getParameter("endPoint");
+        boolean result = SQLRouteHistoryFacade.addRouteToRouteHistory(
+                Route.builder()
+                        .setCustomerId(customerId)
+                        .setDriverId(driverId)
+                        .setStartPoint(startPoint)
+                        .setEndPoint(endPoint)
+                        .build());
         setTextType(resp);
-        resp.getWriter().write("Success");
+        if (result) {
+            resp.getWriter().write("Success");
+        } else {
+            resp.getWriter().write("Error");
+        }
     }
 }
